08c6d6379ad5aa551781a9f80ceacf4f700ab04d
Devanshu <devanshu@gmail.com>
2026-04-22 20:58:59 +0530

    db connect

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
+// 🔹 1. ADD THIS AT TOP (DB connection)
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'database-1.c7kwqwcoscap.ap-south-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'yourpass',
+  database: 'database-1'
+});
+
+db.connect(err => {
+  if (err) {
+    console.log("DB error ❌");
+  } else {
+    console.log("DB connected ✅");
+  }
+});
+
 const express = require('express');
 const app = express();
 
